059915ec5dd6268f27d3f4bd7b3cc701ad7a9352
cotty <kouhei03620@yahoo.co.jp>
2017-07-21 23:57:29 +0000

    restart

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  #コントローラーがdeviseのものならばproetctedにあるストロングパラメータ設定を実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parameters
+    #create用 ストロングパラメータ
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #edit用ストロングパラメータ
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+end
